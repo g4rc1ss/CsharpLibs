6c9f3803e87a6501060019a780ee3c87b8ee7ef7
g4rc1ss <garciss@protonmail.com>
2019-10-10 00:04:56 +0200

    Create modulos para Benchmarking

---
+++ CsharpLibs/03_BenchMarking/01_NetStandard/BenchMarkingDirectoriosArchivos/BenchDirectoriosArchivos.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchMarkingDirectoriosArchivos {
+    [InProcess]
+    public class BenchDirectoriosArchivos {
+        [Benchmark()]
+        public void BenchOrdenar() => new TestDirectoriosArchivos.TestArchivos().Ordenar();
+
+        [Benchmark()]
+        public void BenchCopiar() => new TestDirectoriosArchivos.TestDirectorios().Copiar();
+    }
+}
