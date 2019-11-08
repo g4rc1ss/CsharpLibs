f862616aea773ea8895fad3ab396f9f6b63f7c4d
g4rc1ss <garciss@protonmail.com>
2019-11-08 14:10:51 +0100

    Feature/modificar arquitectura c sharp libs (#17)
    * update arquitectura
    
    * update arquitectura
    
    Actualizo toda la arquitectura, cambio de ubicacion de los archivos y proyectos y cambio de nombres de proyectos
    
    * update namespaces de los archivos
    
    Actualizamos los namespaces y ubicacion de ciertas clases y archivos para el uso de las librerias :person_with_blond_hair:
    
    * actualizacion de dependencias
    
    * Update to Core 3.0
    
    * chage DirectorioArchivos to Data
    
    * correguir error de namespace
    
    * actualizar nombres de carpetas de proyectos

---
+++ CsharpLibs/03_BenchMarking/Data/Core.Data.BenchMarkingLogs/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Core.Data.BenchMarkingLogs {
+    internal class Program {
+        private static void Main(string[] args) {
+            BenchmarkRunner.Run<BenchFicheroLog>();
+        }
+    }
+}
