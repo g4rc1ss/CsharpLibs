0955888689abb2755aad40174087663fe9fa70dc
g4rc1ss <garciss@protonmail.com>
2019-11-08 12:54:53 +0100

    Feature/modificar arquitectura c sharp libs (#16)
    * update arquitectura
    
    * update arquitectura
    
    Actualizo toda la arquitectura, cambio de ubicacion de los archivos y proyectos y cambio de nombres de proyectos
    
    * update namespaces de los archivos
    
    Actualizamos los namespaces y ubicacion de ciertas clases y archivos para el uso de las librerias :person_with_blond_hair:
    
    * actualizacion de dependencias
    
    * Update to Core 3.0
    
    * chage DirectorioArchivos to Data

---
+++ CsharpLibs/02_Test/Data/TestDirectoriosArchivos/TestArchivos.cs
@@ -0,0 +1,18 @@
+﻿using Core.Data.DirectoriosArchivos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace TestDirectoriosArchivos {
+    [TestClass]
+    public class TestArchivos {
+
+        [TestMethod]
+        public void Ordenar() {
+            string[] archivos = Directory.GetDirectories("prueba", "", SearchOption.AllDirectories);
+
+            var listaOrdenada = Archivos.FicherosOrdenados(archivos);
+
+            Assert.IsTrue(listaOrdenada.Count == archivos.Length);
+        }
+    }
+}
