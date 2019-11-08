6c14c39015f1f3ba67c4300464278299db2c9922
g4rc1ss <garciss@protonmail.com>
2019-11-08 21:52:41 +0100

    Borrar BenchMarking

--- CsharpLibs/03_BenchMarking/Common/Core.Common.BenchMarkingRespuesta/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-
-namespace Core.Common.BenchMarkingRespuesta {
-    internal class Program {
-        private static void Main(string[] args) {
-            BenchmarkRunner.Run<BenchRespuesta>();
-        }
-    }
-}
--- CsharpLibs/03_BenchMarking/Data/Core.Data.BenchMarkingFiles/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-
-namespace Core.Data.BenchMarkingFiles {
-    internal class Program {
-        private static void Main(string[] args) {
-            BenchmarkRunner.Run<BenchFiles>();
-        }
-    }
-}
