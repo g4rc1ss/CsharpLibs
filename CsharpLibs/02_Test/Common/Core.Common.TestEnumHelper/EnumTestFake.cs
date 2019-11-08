ce85f0a31b0f4c2995edada7c67d772926eb9de2
g4rc1ss <garciss@protonmail.com>
2019-11-08 23:28:19 +0100

    Feature/libreria enumeradores (#24)
    * Update .editorconfig
    
    * Crear libreria para manejo de Enums
    
    Libreria para mejorar la experiencia del uso de los enumeradores
    
    * Test de la libreria de enumeradores
    
    * Create Core.Common.TestEnumHelper.csproj
    
    * update version de los paquetes nuget
    
    Actualizar el paquete nuget para implementar la libreria
    
    * Creacion y ejecucion de Test y cambio de algo de docu de la libreria de enumFactory
    
    * update Cambio de ubicacion de test fisico
    
    * Update CsharpLibs.sln
    
    * Update EnumFactory.cs
    
    * Create Extensions.cs

---
+++ CsharpLibs/02_Test/Common/Core.Common.TestEnumHelper/EnumTestFake.cs
@@ -0,0 +1,23 @@
+﻿using Core.Common.EnumHelper;
+
+namespace Core.Common.TestEnumHelper {
+    public enum EnumTestFake {
+        unknow = -1,
+        [EnumDescription(@"00")]
+        testNumCero = 0,
+        [EnumDescription(@"10")]
+        testNumUno = 1,
+        [EnumDescription(@"Dos")]
+        testNumDos = 2,
+        [EnumDescription(@"hgfjrhguor")]
+        testNumDiez = 3,
+    }
+
+    public enum EnumTestFakeSinClaves {
+        desconocido = -1,
+        enumSinClave0 = 0,
+        enumSinClave1 = 1,
+        enumSinClave2 = 2,
+        enumSinClave3 = 3
+    }
+}
