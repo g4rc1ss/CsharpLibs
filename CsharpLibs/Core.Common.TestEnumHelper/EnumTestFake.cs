1c364b5ec519b4c48419d715e5b405ea304cec3d
g4rc1ss <garciss@protonmail.com>
2019-11-08 20:30:45 +0100

    Test de la libreria de enumeradores

---
+++ CsharpLibs/Core.Common.TestEnumHelper/EnumTestFake.cs
@@ -0,0 +1,29 @@
+﻿using Core.Common.EnumHelper;
+
+namespace Core.Common.TestEnumHelper {
+    public enum EnumTestFake {
+        unknow = -1,
+        [EnumDescription(@"00")]
+        testNumCero = 0,
+        [EnumDescription(@"10")]
+        testNumUno = 1,
+        [EnumDescription(@"25")]
+        testNumDos = 2,
+        [EnumDescription(@"tres")]
+        testNumTres = 3,
+        [EnumDescription(@"cuatro")]
+        testNumCuatro = 4,
+        [EnumDescription(@"cinco")]
+        testNumCinco = 5,
+        [EnumDescription(@"seis")]
+        testNumSeis = 6,
+        [EnumDescription(@"siete07")]
+        testNumSiete = 7,
+        [EnumDescription(@"080")]
+        testNumOcho = 8,
+        [EnumDescription(@"1000")]
+        testNumNueve = 9,
+        [EnumDescription(@"hgfjrhguor")]
+        testNumDiez = 10,
+    }
+}
---
+++ CsharpLibs/Core.Common.TestEnumHelper/TestConversorEnumeradores.cs
@@ -0,0 +1,17 @@
+﻿using Core.Common.EnumHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Common.TestEnumHelper {
+    [TestClass]
+    public class TestConversorEnumeradores {
+        [TestMethod]
+        public void TestEnumItemFromKey() {
+            var enumeradorPrueba = EnumFactory.EnumItemFromKey<EnumTestFake>("02");
+        }
+
+        [TestMethod]
+        public void TestKeyFromEnumItem() {
+            var clavePrueba = EnumFactory.KeyFromEnumItem<EnumTestFake>(EnumTestFake.unknow);
+        }
+    }
+}
