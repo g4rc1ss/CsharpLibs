945bd7164066ddacb441bc188da4a534432bf7de
g4rc1ss <garciss@protonmail.com>
2019-11-12 21:21:42 +0100

    Feature/script dependencias nuspec powershell (#29)
    * ajustes y formatear sintaxis
    
    * Create ActualizarDependenciasNuspec.ps1
    
    * subir cambiios
    
    Script funciona, ahora quiero hacer mas sencilla la modificacion de parametros como el id, autores, rutas, etc
    
    * crear script para generar documento nuspec
    
    * Update CsharpLibs.sln
    
    * correccion de error logico de rutas
    
    * Update CI-CD_Merge.yml
    
    * Update CI-CD_Merge.yml

--- CsharpLibs/01_CsharpLibs/Common/Core.Common.Respuestas/RespuestaT.cs
+++ CsharpLibs/01_CsharpLibs/Common/Core.Common.Respuestas/RespuestaT.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Core.Common.Respuestas {
     public class Respuesta<T> :Respuesta {
@@ -10,18 +8,18 @@ namespace Core.Common.Respuestas {
         /// </summary>
         public T Datos { get; private set; }
 
-        public Respuesta() : base() {  }
+        public Respuesta() : base() { }
 
         public Respuesta(T datos) : base() {
             Datos = datos;
         }
 
-        public Respuesta(int resultado, string mensaje, string funcionalidad) 
+        public Respuesta(int resultado, string mensaje, string funcionalidad)
             : base(resultado, mensaje, funcionalidad) {
         }
 
         public Respuesta(Exception ex, string funcionalidad = "", bool guardarLog = true, DondeGuardar donde = DondeGuardar.ArchivoTexto)
-            :base(ex, funcionalidad, guardarLog, donde) {
+            : base(ex, funcionalidad, guardarLog, donde) {
 
         }
     }
