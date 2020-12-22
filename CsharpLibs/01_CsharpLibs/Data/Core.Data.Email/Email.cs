5596bed6974d2f3a60708f4e5642ffc739769b4a
g4rc1ss <garciss@protonmail.com>
2020-12-22 17:09:43 +0100

    Implementar Envio de Correo

---
+++ CsharpLibs/01_CsharpLibs/Data/Core.Data.Email/Email.cs
@@ -0,0 +1,65 @@
+﻿using Core.Common.Respuestas;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Core.Data.Email {
+    public abstract class Email {
+        private readonly string regexCuerpoVariables = "¬V\\d+";
+        private readonly string rutaUbicacionPlantillasHtml = @"C:\PlantillasEmail\";
+
+        public string ServidorEnvio { get; set; }
+        public string Remitente { get; set; }
+        public string Destinatario { get; set; }
+        public string Asunto { get; set; }
+        public string NombrePlantilla { get; set; }
+        public string Cuerpo { get; set; }
+        public List<string> BodyPersonalizado { get; set; }
+        public List<byte[]> ArchivosAdjuntos { get; set; }
+        public List<string> NombreArchivosAdjunto { get; set; }
+
+        public string Usuario { get; }
+        public string Password { get; }
+
+        public Email(string usuario, string password, string rutaUbicacionPlantilla = "") {
+            Usuario = usuario;
+            Password = password;
+            if (!string.IsNullOrEmpty(rutaUbicacionPlantilla))
+                rutaUbicacionPlantillasHtml = rutaUbicacionPlantilla;
+        }
+
+        public abstract Respuesta Enviar();
+
+        internal void InicializarEnvioEmail() {
+            ValidarCampos();
+            SustituirTokens();
+        }
+
+        private void ValidarCampos() {
+            if (string.IsNullOrEmpty(Remitente))
+                throw new Exception($"Campo {nameof(Remitente)} vacio");
+            if (string.IsNullOrEmpty(Destinatario))
+                throw new Exception($"Campo {nameof(Destinatario)} vacio");
+            if (string.IsNullOrEmpty(Usuario))
+                throw new Exception($"Campo {nameof(Usuario)} vacio");
+            if (string.IsNullOrEmpty(Password))
+                throw new Exception($"Campo {nameof(Password)} vacio");
+            if (string.IsNullOrEmpty(ServidorEnvio))
+                throw new Exception($"Campo {nameof(ServidorEnvio)} vacio");
+            if (ArchivosAdjuntos?.Count != NombreArchivosAdjunto?.Count)
+                throw new Exception("Los archivos tienen que tener su nombre correspondiente");
+        }
+
+        private void SustituirTokens() {
+            var regex = new System.Text.RegularExpressions.Regex(regexCuerpoVariables);
+            var cuerpo = ObtenerTextoDesdeRepositorio();
+
+            foreach (var param in BodyPersonalizado) cuerpo = regex.Replace(cuerpo, param, 1);
+            Cuerpo = cuerpo;
+        }
+
+        private string ObtenerTextoDesdeRepositorio() {
+            return File.ReadAllText(Path.Combine(rutaUbicacionPlantillasHtml, NombrePlantilla));
+        }
+    }
+}
---
+++ CsharpLibs/01_CsharpLibs/Data/Core.Data.Email/SMTP/EmailSmtp.cs
@@ -0,0 +1,31 @@
+﻿using Core.Common.Respuestas;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+namespace Core.Data.Email.SMTP {
+    public class EmailSmtp :Email {
+        public EmailSmtp(string usuario, string password, string rutaUbicacionPlantilla = "") : base(usuario, password, rutaUbicacionPlantilla) {
+        }
+
+        public override Respuesta Enviar() {
+            InicializarEnvioEmail();
+            using (var cliente = new SmtpClient(ServidorEnvio)) {
+                cliente.EnableSsl = true;
+                cliente.Credentials = new NetworkCredential(Usuario, Password);
+                var mensaje = new MailMessage(Remitente, Destinatario) {
+                    IsBodyHtml = true,
+                    Subject = Asunto,
+                    Body = Cuerpo,
+                };
+
+                if (ArchivosAdjuntos == null || ArchivosAdjuntos.Count > 0)
+                    using (var stream = new MemoryStream()) for (var x = 0; x < ArchivosAdjuntos.Count; x++)
+                            mensaje.Attachments.Add(new Attachment(stream, NombreArchivosAdjunto[x]));
+
+                cliente.Send(mensaje);
+            }
+            return new Respuesta();
+        }
+    }
+}
