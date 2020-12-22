3042d2619800d2d24448d58aa3be91d068cb1157
g4rc1ss <garciss@protonmail.com>
2020-12-22 19:22:04 +0100

    Crear Test de Email

---
+++ CsharpLibs/02_Test/Data/Core.Data.TestEmail/TestEmailSMTP.cs
@@ -0,0 +1,14 @@
+﻿using Core.Data.Email.SMTP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Data.TestEmail {
+    [TestClass]
+    public class TestEmailSMTP {
+        [TestMethod]
+        public void SendEmail() {
+            new EmailSmtp("", "", "") {
+
+            };
+        }
+    }
+}
