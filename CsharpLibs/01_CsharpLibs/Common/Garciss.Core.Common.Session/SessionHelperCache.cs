d3491fc74aaaa05f67b7e40dd0bacec23bebee7c
g4rc1ss <garciss@protonmail.com>
2020-12-23 11:29:45 +0100

    Crear SessionCache

---
+++ CsharpLibs/01_CsharpLibs/Common/Garciss.Core.Common.Session/SessionHelperCache.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Garciss.Core.Common.Session {
+    /// <summary>
+    /// Libreria para implementar los objetos en cache, se usa el objeto ISession
+    /// Por tanto hay que configurar el Startup de la siguiente manera:
+    /// En la funcion de ConfigureServices
+    /// services.AddSession(opts => {
+    ///     opts.Cookie.IsEssential = true;
+    ///     opts.Cookie.HttpOnly = true;
+    ///     opts.Cookie.Name = "EguzkimendiSession";
+    /// });
+    /// services.AddMemoryCache();
+    /// 
+    /// En la funcion de Configure
+    /// app.UseSession();
+    /// </summary>
+    public static class SessionHelperCache {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void SetObjectAsJson(this ISession session, string key, object value) {
+            var str = JsonConvert.SerializeObject(value);
+            session.SetString(key, str);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="session"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T GetObjectFromJson<T>(this ISession session, string key) {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool RemoveActivities(this ISession session, string key) {
+            if (session != null) {
+                session.Remove(key);
+                return true;
+            }
+            return false;
+        }
+    }
+}
