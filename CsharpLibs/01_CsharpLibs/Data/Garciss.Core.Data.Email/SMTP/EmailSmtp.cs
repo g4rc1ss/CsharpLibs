fc9060c19d445fdfa41e298bd3d84b6165741ecb
g4rc1ss <garciss@protonmail.com>
2021-01-20 20:25:38 +0100

    update

--- CsharpLibs/01_CsharpLibs/Data/Garciss.Core.Data.Email/SMTP/EmailSmtp.cs
+++ CsharpLibs/01_CsharpLibs/Data/Garciss.Core.Data.Email/SMTP/EmailSmtp.cs
@@ -2,12 +2,14 @@
 using System.IO;
 using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace Garciss.Core.Data.Email.SMTP {
     public class EmailSmtp :Email {
         public EmailSmtp(string servidor, string usuario, string password, string rutaUbicacionPlantillasHtml)
             : base(servidor, usuario, password, rutaUbicacionPlantillasHtml) {
         }
+
         /// <summary>
         /// Despues de inicializar los datos se podra usar el metodo Enviar que preparará el envio del email
         /// </summary>
@@ -35,20 +37,36 @@ namespace Garciss.Core.Data.Email.SMTP {
         /// </example>
         public override Respuesta Enviar() {
             InicializarEnvioEmail();
-            using (var cliente = new SmtpClient(servidorEnvio)) using (var stream = new MemoryStream()) {
-                cliente.EnableSsl = SSL;
-                cliente.Credentials = new NetworkCredential(usuario, password);
-                var mensaje = new MailMessage(Remitente, Destinatario) {
-                    IsBodyHtml = true,
-                    Subject = Asunto,
-                    Body = Cuerpo,
-                };
 
-                if (ArchivosAdjuntos != null && ArchivosAdjuntos?.Count > 0)
-                    for (var x = 0; x < ArchivosAdjuntos.Count; x++) mensaje.Attachments.Add(new Attachment(stream, NombreArchivosAdjunto[x]));
-                cliente.Send(mensaje);
-            }
+            if (Destinatarios.Count == 1)
+                EnviarMensaje(Destinatarios[0]);
+            else
+                Parallel.ForEach(Destinatarios, destinatario => {
+                    EnviarMensaje(destinatario);
+                });
+
             return new Respuesta();
+        }
+
+        private void EnviarMensaje(string destinatario) {
+            using (var cliente = new SmtpClient(servidorEnvio)) {
+                using (var stream = new MemoryStream()) {
+                    cliente.EnableSsl = SSL;
+                    cliente.Credentials = new NetworkCredential(usuario, password);
+
+                    var mensaje = new MailMessage(Remitente, destinatario) {
+                        IsBodyHtml = true,
+                        Subject = Asunto,
+                        Body = Cuerpo,
+                    };
+
+                    if (ArchivosAdjuntos != null && ArchivosAdjuntos?.Count > 0)
+                        for (var x = 0; x < ArchivosAdjuntos.Count; x++) {
+                            mensaje.Attachments.Add(new Attachment(stream, NombreArchivosAdjunto[x]));
+                        }
+                    cliente.Send(mensaje);
+                }
+            }
         }
     }
 }
