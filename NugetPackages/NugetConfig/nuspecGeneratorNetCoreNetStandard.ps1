ef9f925174c6bbb4f7763b1344cc2ab5bfe472ca
g4rc1ss <garciss@protonmail.com>
2021-08-28 16:25:25 +0200

    Revert "Update Csharp to Net"
    This reverts commit 2505f1263a767b43e2231993616d06cc9d87dc04.

--- NugetPackages/NugetConfig/nuspecGeneratorNetCoreNetStandard.ps1
+++ NugetPackages/NugetConfig/nuspecGeneratorNetCoreNetStandard.ps1
@@ -24,7 +24,7 @@ $rutaDLL = "../PackagesCompilaciones/"
 
 ## Datos del paquete Nuget
 $id = "Garciss.Net.Libs"
-$title = "Garciss Net Libs"
+$title = "Garciss Csharp Libs"
 $authors = "Asier Garcia"
 $owners = "Asier Garcia"
 $requireLicenseAcceptance = "false"
